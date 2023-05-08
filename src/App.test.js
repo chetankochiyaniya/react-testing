826d247a4f01e8be86aedf426db3e9fdaeecc85a
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-05-08 16:00:43 +0530

    new test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//    // const linkElement = screen.getByText(/learn react/i);
+//   // update test from regEx to literal string
+//   const linkElement = screen.getByText('Learn React');
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// bank test will pass
 test('renders learn react link', () => {
-  render(<App />);
-  // update test from regEx to literal string
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
 });
+
+// throw error
+test('renders learn react link', () => {
+  throw new Error('test failed')
+});
+
+// new test case
+test("baki sab chal raha he",()=>{
+  console.log("test");
+})
